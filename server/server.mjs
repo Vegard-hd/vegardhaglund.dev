4142690c0a7ef806639b405a3f16dd3582073bcc
vegard-hd <vegard.hd@outlook.com>
2025-03-07 19:32:02 +0100

    updated express server with new error handling / 404 page

--- server/server.mjs
+++ server/server.mjs
@@ -1,16 +1,48 @@
-import express, { static as s } from "express";
+import express, { static as static_ } from "express";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
 
+const pathing = join(__dirname, "../", "dist");
+console.log("pathing is .... ", pathing);
+
 // Serve static files from the dist directory
-app.use(s(join(__dirname, "../", "dist")));
+app.use(static_(join(__dirname, "../", "dist")));
 
 // Send the main HTML file for any other requests
-app.get("*", (req, res) => {
-  res.sendFile(join("__dirname", "dist", "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "../", "dist", "index.html"));
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  console.warn("404 handler called for", req.url);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error); // Pass the error to the next middleware
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  console.error("Error handler called:", err.message); // Log the error
+
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Render the error page (or send JSON)
+  res.status(err.status || 500); // Set the status code
+
+  //Check if the client accepts HTML
+  if (req.accepts("html")) {
+    res.sendFile(join(__dirname, "error.html")); // Serve the error page
+  } else {
+    // Respond with JSON if the client requested JSON
+    res.json({ error: { message: err.message, status: err.status || 500 } });
+  }
 });
 
 app.listen(port, () => {
