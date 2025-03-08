497a8ffa4afc0c7d575b0d0021a635c672c135cd
vegard-hd <vegard.hd@outlook.com>
2025-03-08 23:21:15 +0100

    changes to font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,23 +1,58 @@
 export function Hero() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse bg-base-300 rounded">
-        <img
-          className="max-w-sm rounded-lg shadow-2xl"
-          src="../../public/logo.webp"
-          alt="Site logo generated with AI"
-          srcSet="/logo.webp"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <>
+      <h2 className="flex justify-self-center text-3xl mt-25 mb-5  sm:mb-1 text-base-400 font-semibold text-center ">
+        My Projects
+      </h2>
+      <div className="flex w-full flex-col items-center justify-center md:h-96 sm:h-auto mb-20">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-6xl">
+          <div className="card card-side bg-base-300 shadow-sm ">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                alt="Movie"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">New movie is released!</h2>
+              <p>Click the button to watch on Jetflix app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Watch</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-side bg-base-100 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                alt="Movie"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">New movie is released!</h2>
+              <p>Click the button to watch on Jetflix app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Watch</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-side bg-base-100 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                alt="Movie"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">New movie is released!</h2>
+              <p>Click the button to watch on Jetflix app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Watch</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,7 +1,7 @@
 export function Title() {
   return (
     <>
-      <h1 className="text-center text-primary bg-base-200 text-4xl md:text-5xl lg:text-7xl pt-10 pb-6 z-50">
+      <h1 className=" text-center text-primary bg-base-200 text-4xl md:text-5xl lg:text-7xl pt-10 pb-6 z-50">
         VegardHaglund.dev
       </h1>
     </>
